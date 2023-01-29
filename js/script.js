1ad408c6bf575e624157606cb16f1be450dce597
Sagar12113 <123369844+Sagar12113@users.noreply.github.com>
2023-01-29 11:34:49 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,23 +1,59 @@
 const counterNum = document.querySelectorAll(".count_number");
 const speed = 5;
 
-counterNum.forEach((curElement) =>{
-    const updateNumber = () => {
-        const targetNumber = parseInt(curElement.dataset.number);
-        // console.log(targetNumber);
-        const initialNum = parseInt(curElement.innerText);
-        // console.log(initialNum);
+// counterNum.forEach((curElement) =>{
+//     const updateNumber = () => {
+//         const targetNumber = parseInt(curElement.dataset.number);
+//         // console.log(targetNumber);
+//         const initialNum = parseInt(curElement.innerText);
+//         // console.log(initialNum);
 
-        const incrementNumber = Math.trunc(targetNumber / speed);
-        // console.log(incrementNumber);
-        if (initialNum < targetNumber) {
-            curElement.innerText = `${initialNum + incrementNumber }+ `;
-            setTimeout(updateNumber, 300)
-        }
+//         const incrementNumber = Math.trunc(targetNumber / speed);
+//         // console.log(incrementNumber);
+//         if (initialNum < targetNumber) {
+//             curElement.innerText = `${initialNum + incrementNumber }+ `;
+//             setTimeout(updateNumber, 300)
+//         }
 
 
-    };
-    updateNumber();
+//     };
+//     updateNumber();
+// });
+
+
+$('.count_1').each(function () {
+  $(this).prop('Counter',0).animate({
+      Counter: $(this).text()
+  }, {
+      duration: 4000,
+      easing: 'swing',
+      step: function (now) {
+          $(this).text(Math.ceil(now));
+          
+      }
+  });
+});
+$('.count_2').each(function () {
+  $(this).prop('Counter',0).animate({
+      Counter: $(this).text()
+  }, {
+      duration: 4000,
+      easing: 'swing',
+      step: function (now) {
+          $(this).text(Math.ceil(now));
+      }
+  });
+});
+$('.count_3').each(function () {
+  $(this).prop('Counter',0).animate({
+      Counter: $(this).text()
+  }, {
+      duration: 4000,
+      easing: 'swing',
+      step: function (now) {
+          $(this).text(Math.ceil(now));
+      }
+  });
 });
 
 
@@ -48,4 +84,7 @@ const config = {
   var video = document.querySelector('video');
   video.muted = true;
   video.play()
+
+
+
   
